4d2cb3969f00c2fefd8a2b94bd48bff52b69c6b0
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2025-03-04 19:06:33 +0300

    Uygulama Server Tanıtımı - 4

--- backend/src/controllers/auth/validations.js
+++ backend/src/controllers/auth/validations.js
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
-export const Schema = Joi.object({
+export const UserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(3).required(),
 });
 
-export default Schema;
+export default UserSchema;
