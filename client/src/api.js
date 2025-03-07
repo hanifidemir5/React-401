a7b3e1a5de0b7c89f7d5c411f4f719cc4402ed7d
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2025-03-08 01:05:38 +0300

    Detay Ekranlarının Hazırlanması - 2

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const fetchProductList = async () => {
-  const { data } = await axios("http://localhost:5000/product");
+  const { data } = await axios(`${process.env.REACT_APP_BASE_ENDPOINT}/product`);
   return data;
 };
 
 export const fetchProduct = async (product_id) => {
-  const { data } = await axios(`http://localhost:5000/product/${product_id}`);
+  const { data } = await axios(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`);
   return data;
 };
--- client/src/pages/ProductDetail/index.js
+++ client/src/pages/ProductDetail/index.js
@@ -4,13 +4,15 @@ import { useQuery } from "react-query";
 import { fetchProduct } from "../../api";
 import { Box, Text, Button } from "@chakra-ui/react";
 import moment from "moment";
+import ImageGallery from "react-image-gallery";
+
 const ProductDetail = (props) => {
   const { product_id } = useParams();
   const { isLoading, isError, data } = useQuery(["product", product_id], () => fetchProduct(product_id));
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error...</div>;
-
+  const images = data.photos.map((url) => ({ original: url }));
   return (
     <Box>
       <Button colorScheme={"pink"}>Add to Basket</Button>
@@ -19,6 +21,9 @@ const ProductDetail = (props) => {
       </Text>
       <Text>{moment(data.createdAt).format("DD/MM/YYYY")}</Text>
       <p>{data.description}</p>
+      <Box margin={"10"}>
+        <ImageGallery items={images} showThumbnails={false} />
+      </Box>
     </Box>
   );
 };
