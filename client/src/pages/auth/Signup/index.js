0e699cad28186879de3d17a108ec946b6fb1d82b
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2025-03-08 18:03:47 +0300

    Kullanıcı Kayıt İşlemleri - 2

--- client/src/pages/auth/Signup/index.js
+++ client/src/pages/auth/Signup/index.js
@@ -1,29 +1,49 @@
 import React from "react";
 import { Box, Flex, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import validationSchema from "./validations";
 
-const Signup = (props) => {
+const Signup = () => {
   return (
-    <Flex align={"center"} w={"full"} justifyContent={"center"}>
+    <Flex align="center" w="full" justifyContent="center">
       <Box pt={10}>
-        <Box textAlign={"center"}>
-          <Heading size={"5xl"}>Sign up</Heading>
+        <Box textAlign="center">
+          <Heading size="2xl">Sign up</Heading>
         </Box>
-        <Box my={5} textAlign={"left"}>
-          <FormControl>
-            <FormLabel>Username</FormLabel>
-            <Input name="email" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Password</FormLabel>
-            <Input name="password" type="password" />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Password Confirm</FormLabel>
-            <Input name="passwordConfirm" type="passwordConfirm" />
-          </FormControl>
-          <Button mt={4} w={"full"} type="submit">
-            Sign up
-          </Button>
+        <Box my={5} textAlign="left">
+          <Formik
+            initialValues={{ email: "", password: "", passwordConfirm: "" }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              console.log(values);
+            }}
+          >
+            {({ isSubmitting, isValid, dirty }) => (
+              <Form>
+                <FormControl>
+                  <FormLabel>Email</FormLabel>
+                  <Field as={Input} name="email" />
+                  <ErrorMessage name="email" component="div" />
+                </FormControl>
+
+                <FormControl>
+                  <FormLabel>Password</FormLabel>
+                  <Field as={Input} name="password" type="password" />
+                  <ErrorMessage name="password" component="div" />
+                </FormControl>
+
+                <FormControl>
+                  <FormLabel>Password Confirm</FormLabel>
+                  <Field as={Input} name="passwordConfirm" type="password" />
+                  <ErrorMessage name="passwordConfirm" component="div" />
+                </FormControl>
+
+                <Button mt={4} w="full" type="submit" isDisabled={isSubmitting || !isValid || !dirty}>
+                  Sign up
+                </Button>
+              </Form>
+            )}
+          </Formik>
         </Box>
       </Box>
     </Flex>
